4aae32905071e67bbfba4fc70184ecffba39cf2c
Gary Clarke <garyclarke1974@gmail.com>
2025-10-28 15:49:35 +0000

    Update Year field JSON tag to use year naming

--- internal/data/book.go
+++ internal/data/book.go
@@ -6,5 +6,5 @@ type Book struct {
 	ID     int64  `json:"id"`
 	Title  string `json:"title"`
 	Author string `json:"author,omitempty"`
-	Year   int    `json:",omitempty"`
+	Year   int    `json:"year,omitempty"`
 }
